3930665803c98916399b91399c72685eb261671d
naman1919 <ndengre@bestpeers.com>
2018-04-18 11:47:44 +0530

    previous commited files

--- shop_data.rb
+++ shop_data.rb
@@ -1,87 +1,78 @@
+# frozen_string_literal: true
+
+$LOAD_PATH << '.'
+require 'shop_module'
 require 'time'
 
-$LOAD_PATH << "."
-require "shop_module"
-
+# Give User Details
 class User
   include ShopDetails
   def display_user_details
-    user_details
-    puts "----------USER DETAILS-------------"
-
-    @user_data.each do |detail|
+    users_details.each do |detail|
       detail.each do |key, value|
         puts "#{key} -> #{value}"
-        
-        if value == "buyer"
+        if value == 'buyer'
           display_buyer_name detail
           display_buyer_age detail
-        elsif value == "seller"
+        elsif value == 'seller'
           display_seller_name detail
-          display_seller_age detail 
-        end
-      end
-      print "\n"
-    end
+          display_seller_age detail
+        end; end; end
   end
 
-  ['buyer', 'seller'].each do |user|
+  %w[buyer seller].each do |user|
     define_method("display_#{user}_name") do |detail|
-      puts "FULL NAME->"+detail[:first_name]+" "+detail[:last_name]
+      puts 'FULL NAME->' + detail[:first_name] + ' ' + detail[:last_name]
     end
-  end
 
-  ['buyer', 'seller'].each do |user|
     define_method("display_#{user}_age") do |detail|
       date_of_birth = detail[:date_of_birth]
       age = Time.now.year - Time.parse(date_of_birth).year
-      puts detail[:first_name]+" "+detail[:last_name]+"'s age->"+age.to_s+"years old"
+      puts detail[:first_name] + ' ' + detail[:last_name] +
+           "'s age->" + age.to_s + 'years old'
     end
   end
 
   def find_details
-    user_details
-
-    @user_data.each do |detail|
-      detail.each { |key, value| puts "#{key} -> #{value}" if detail.has_value?("alex")}
+    users_details.each do |data|
+      data.each { |key, value| puts "#{key}: #{value}" if data.value?('alex') }
       print "\n"
     end
   end
 end
 
+# Give Product Details
 class Product
   include ShopDetails
-  
   def display_product_details
-    product_details
-    puts "----------------PRODUCT DETAILS------------------"
+    puts 'PRODUCT DETAILS'
 
-    @product_details.each do |detail|
-      detail.each { |key, value| puts "#{ key } -> #{value}"}
+    products_details.each do |data|
+      data.each { |key, value| puts "#{key} -> #{value}" }
       print "\n"
     end
   end
 end
 
+# Give Buyer Details
 class Buyer < User
   def buyer_details
-    user_details
-    puts "---------------------------------BUYER DETAILS-----------------------"
+    puts 'BUYER DETAILS'
 
-    @user_data.each do |detail|
-      detail.each { |key, value| puts "#{key} -> #{value}" if detail.has_value?("buyer") }
+    users_details.each do |data|
+      data.each { |key, value| puts "#{key}: #{value}" if data.value?('buyer') }
       print "\n"
     end
   end
 end
 
+# Give Seller Details
 class Seller < User
   def seller_details
-    user_details
-    puts "---------------------------------SELLER DETAILS-----------------------"
+    puts 'SELLER DETAILS'
 
-    @user_data.each do |detail|
-      detail.each { |key, value| puts "#{key} -> #{value}" if detail.has_value?("seller") }
+    users_details.each do |data|
+      data.each { |key, value| puts "#{key}:#{value}" if data.value?('seller') }
       print "\n"
     end
   end
@@ -89,7 +80,6 @@ end
 user = User.new
 user.display_user_details
 user.find_details
-
 product = Product.new
 product.display_product_details
 
